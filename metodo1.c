9daa8a0819fc37e02152693c4fa08f9cda0f8c10
Thiago <thiagofp707@gmail.com>
2019-09-03 00:28:44 -0300

    falta multiplicar

--- metodo1.c
+++ metodo1.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
     
 int main(){
-  int mat1x, mat1y, mat2x, mat2y;
+  int n;
 
   // Descrição:
 
@@ -11,66 +10,96 @@ int main(){
   // mat2x - Número de linhas da segunda matriz
   // mat2y - Número de colunas da segunda matriz
 
+  scanf("%d", &n); // Lê a ordem das matrizes
+  printf("\n");
 
-  printf("Informe a ordem da primeira matriz (ex: 2 x 2): \n");
-  scanf("%d%d", &mat1x, &mat1y);
 
-  printf("Informe a ordem da segunda matriz (ex: 2 x 2): \n");
-  scanf("%d%d", &mat2x, &mat2y);
+    int **matriz1 = (int**)malloc(n * sizeof(int*)); // Alocou um vetor de ponteiros onde em cada índice será alocado um vetor
 
-  
-  if (validaOperacao(mat1y, mat2x)){
-    // Aloca a matriz 1 (ixj)
-    // Aloca a matriz 2 (mxn)
-    // Aloca a matriz 3 (ixn)
-
-    int **matriz1 = (int**)malloc(mat1x * sizeof(int*)); // Alocou um vetor de ponteiros onde em cada índice será alocado um vetor
-
-    for (int i = 0; i < mat1x; i++){
-      matriz1[i] = (int*) malloc(mat1y * sizeof(int)); // Alocou os vetores de inteiro
-      for (int j = 0; j < mat1y; j++){
+    for (int i = 0; i < n; i++){
+      matriz1[i] = (int*) malloc(n * sizeof(int)); // Alocou os vetores de inteiro
+      for (int j = 0; j < n; j++){
         matriz1[i][j] = 0; // Inicializa os vetores com 0;
       }
     }
 
-    int **matriz2 = (int**)malloc(mat2x * sizeof(int*));
+    int **matriz2 = (int**)malloc(n * sizeof(int*));
 
-    for (int i = 0; i < mat2x; i++){
-      matriz2[i] = (int*) malloc(mat2y * sizeof(int));
-      for (int j = 0; j < mat2y; j++){
+    for (int i = 0; i < n; i++){
+      matriz2[i] = (int*) malloc(n * sizeof(int));
+      for (int j = 0; j < n; j++){
         matriz2[i][j] = 0;
       }
       
     }
 
-    int **matriz3 = (int**) malloc(mat1x * sizeof(int*));
+    int **matriz3 = (int**) malloc(n * sizeof(int*));
 
-    for (int i = 0; i < mat1x; i++){
-      matriz3[i] = (int*) malloc(mat2y * sizeof(int));
-      for (int j = 0; j < mat2y; j++){
+    for (int i = 0; i < n; i++){
+      matriz3[i] = (int*) malloc(n * sizeof(int));
+      for (int j = 0; j < n; j++){
         matriz3[i][j] = 0;
       }
     }
+
+    leMatriz(matriz1, n);
+    leMatriz(matriz2, n);
+
+    imprimeMatriz(matriz1, n); // Retirar isso
+    imprimeMatriz(matriz2, n); // Retirar isso
+
     
     multiplicarMatrizes(matriz1, matriz2, matriz3);
 
-  }else{
-    printf("A operação não pode ser realizada pois o número de colunas da primeira matriz deve ser igual ao número de linhas da segunda matriz");
-  }
-  
+    imprimeMatriz(matriz3, n); // Imprime a matriz resultante
 
+
+    liberaMemoria(matriz1, n);
+    liberaMemoria(matriz2, n);
+    liberaMemoria(matriz3, n);
+  
 
   return 0;
 }
 
-int validaOperacao(int j, int m){
+// Função inultilizada por conta da minha descoberta que as matrizes são quadradas de mesma ordem
+
+/* int validaOperacao(int j, int m){
   if (j == m){
     return 1;
   }else{
     return 0;
   }
+} */
+
+void leMatriz(int** matriz, int n){
+  for (int i = 0; i < n; i++){
+    for(int j=0; j < n; j++){
+      scanf("%d", &matriz[i][j]);
+    }
+  }
+  printf("\n");
+}
+
+void imprimeMatriz(int** matriz, int n){
+  for (int i = 0; i < n; i++){
+    for(int j=0; j < n; j++){
+      printf("%d", matriz[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
 }
 
 void multiplicarMatrizes(int** matriz1, int** matriz2, int** matriz3){
   //Fazer a função de multiplicar aqui
+  printf("Chegou até aqui\n");
 }
+
+void liberaMemoria(int** matriz, int* linhas){
+  for (int i = 0; i < linhas; i++){
+    free(matriz[i]);
+  }
+  free(matriz);
+}
+
